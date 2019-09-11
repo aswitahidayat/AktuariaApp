8e648a405524675445f4dd4fa8dd0d1ea373e7fc
o7yo76yo <muhammad.agya7@gmail.com>
2019-09-11 11:10:09 +0700

    fix zip search

--- app/Http/Controllers/DataMaster/ZipController.php
+++ app/Http/Controllers/DataMaster/ZipController.php
@@ -9,6 +9,7 @@ use Auth;
 use Illuminate\Http\Request;
 use DataTables;
 use DB;
+use Redirect,Response;
 
 class ZipController extends Controller
 {
@@ -25,65 +26,23 @@ class ZipController extends Controller
     }
 
     public function getzip(Request $request){
-        $query = Zip::
-            leftJoin('kka_dab.mst_village', 'zip_villid', '=', 'kka_dab.mst_village.vill_id')
-            ->leftJoin('kka_dab.mst_sub_district', 'vill_subdisid', '=', 'kka_dab.mst_sub_district.subdis_id')
-            ->leftJoin('kka_dab.mst_district', 'kka_dab.mst_sub_district.subdis_disid', '=', 'kka_dab.mst_district.dis_id')
-            ->leftJoin('kka_dab.mst_province', 'kka_dab.mst_district.dis_provid', '=', 'kka_dab.mst_province.prov_id');
-
-            if($request->provid != ''){
-                $query->where('dis_provid', $request->provid);
-            }
-    
-            if($request->dis != ''){
-                $query->where('dis_id', $request->dis);
-            }
-
-            if($request->subdis != ''){
-                $query->where('subdis_disid', $request->subdis);
-            }
-    
-            if($request->name != ''){
-                $query->where('subdis_name', 'LIKE', "%$request->name%");
-            }
-    
-            if($request->limit != ''){
-                $query->skip(0)->take(10);
-            }
-
-            $data =  $query->get();
-
-            return Datatables::of($data)
-                ->addIndexColumn()
-                ->addColumn('action', function($row){
-                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->zip_id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editZip">Edit</a>';
-                    $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->zip_id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteZip">Delete</a>';
-                    return $btn;
-                })
-                ->addColumn('statusName', function($row){
-                    $btn = $row->zip_status == 1 ? "Active" : "Inactive" ;
-                    return $btn;
-                })
-                ->rawColumns(['action'])
-                ->make(true);
+        $data = $this->zip($request, 'pagging');
+        
+        $b['data']= $data;
+        $b['recordsFiltered']= $this->zip($request, 'count');
+        $b['recordsTotal']= $this->zip($request, 'count');
+        return $b;
     }
 
-    public function edit($zip_id)
+    public function edit($zipcode_id)
     {
-        $data = Zip:: 
-            leftJoin('kka_dab.mst_village', 'zip_villid', '=', 'kka_dab.mst_village.vill_id')
-            ->leftJoin('kka_dab.mst_sub_district', 'vill_subdisid', '=', 'kka_dab.mst_sub_district.subdis_id')
-            ->leftJoin('kka_dab.mst_district', 'kka_dab.mst_sub_district.subdis_disid', '=', 'kka_dab.mst_district.dis_id')
-            ->leftJoin('kka_dab.mst_province', 'kka_dab.mst_district.dis_provid', '=', 'kka_dab.mst_province.prov_id')
-            ->where('zip_id', $zip_id)
-            ->first()
-        ;
+        $data = $this->zip(new Request,'edit', $zipcode_id);
         return response()->json($data);
     }
 
     public function store(Request $request)
     {
-        if($request->zip_id == ''){
+        if($request->zipcode_id == ''){
             $data = new Zip();
             $data->zip_villid = $request->zip_villid;
             $data->zip_code = $request->zip_code;
@@ -93,7 +52,7 @@ class ZipController extends Controller
             $data->save();
         }else{
             DB::table('kka_dab.mst_zipcode')
-                ->where('zip_id', $request->zip_id)
+                ->where('zipcode_id', $request->zipcode_id)
                 ->update(['zip_villid' => $request->zip_villid,
                     'zip_code' => $request->zip_code,
                     'zip_status' => $request->zip_status,
@@ -102,10 +61,59 @@ class ZipController extends Controller
         return response()->json(['success'=>'Zip Type saved successfully.']);
     }
 
-    public function destroy($zip_id)
+    public function destroy($zipcode_id)
     {
-        Zip::find($zip_id)->delete();
+        Zip::find($zipcode_id)->delete();
         return response()->json(['success'=>'Zip Type deleted successfully.']);
     }
+
+    public function zip(Request $request, $reqType ='', $param = ''){
+        $query = Zip::
+            leftJoin('kka_dab.mst_sub_district', 'zip_subdisid', '=', 'kka_dab.mst_sub_district.subdis_id')
+            ->leftJoin('kka_dab.mst_district', 'kka_dab.mst_sub_district.subdis_disid', '=', 'kka_dab.mst_district.dis_id')
+            ->leftJoin('kka_dab.mst_province', 'kka_dab.mst_district.dis_provid', '=', 'kka_dab.mst_province.prov_id');
+
+        $query->raw("zip_status AS total");
+        if($request->provid != ''){
+            $query->where('dis_provid', $request->provid);
+        }
+
+        if($request->dis != ''){
+            $query->where('dis_id', $request->dis);
+        }
+
+        if($request->subdis != ''){
+            $query->where('subdis_disid', $request->subdis);
+        }
+
+        if($request->name != ''){
+            $query->where('subdis_name', 'LIKE', "%$request->name%");
+        }
+
+        // if($request->limit != ''){
+        //     $query->skip(0)->take(10);
+        // }
+        
+        if($reqType == 'pagging'){
+            $length = $request->length != '' ? $request->length : 10;
+            $pag = $query->skip($request->start)->take($length)->get();
+            foreach ($pag as $key =>$value) {
+                $btn = '<a href="javascript:void(0)" 
+                    data-toggle="tooltip"  data-id="'.$value->zipcode_id.'" 
+                    data-original-title="Edit" class="edit btn btn-primary btn-sm editZip">Edit</a>';
+                
+                $pag[$key]->DT_RowIndex = ($key+ 1)+$request->start;
+                $pag[$key]->statusName = 'Active';
+                $pag[$key]->action = $btn;
+            }
+            return $pag;
+        } else if ($reqType == 'get'){
+            return $query->get();
+        } else if ($reqType == 'count'){
+            return $query->count();
+        } else if ($reqType == 'edit'){
+            return $query->where('zipcode_id', $param)->first();
+        }
+    }   
 
 }
